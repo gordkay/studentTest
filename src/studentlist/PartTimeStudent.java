5884ffddf9c1eba8263fb64d3de82099a9e99ec8
Kayla Gordon <gordkayl@sheridancollege.ca>
2024-09-24 00:19:18 -0400

    new part time student class

---
+++ src/studentlist/PartTimeStudent.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studentlist;
+
+/**
+ *
+ * @author Dell
+ */
+public class PartTimeStudent extends Student {
+    
+    public int getNumCourses(){
+    return numCourses;
+    }
+    
+    public void setNumCourses(int numCourses){
+    this.numCourses = numCourses;
+    }
+    
+}
